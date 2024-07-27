99041b315f4e80c99ccc03763614835bcc0f3a23
Mugao-joy <mugaojoy@gmail.com>
2024-07-27 19:16:48 +0300

    added open api

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,4 +1,9 @@
 import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
 //connection and listeners
-app.listen(5000, () => console.log('Server Open'));
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log('Server Open & Connected To Database'));
+}).catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
