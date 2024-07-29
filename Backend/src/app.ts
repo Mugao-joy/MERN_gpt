9f36dee3f9397013dc43d3b82c3a694503dfe69f
Mugao-joy <mugaojoy@gmail.com>
2024-07-29 16:59:58 +0300

    created routes for users and chats

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from 'morgan' //gives log description of what type of request was made, response and status code
+import appRouter from "./routes/index.js";
 config()
 
 const app = express()
 
 //middlewares
-app.use(express.json())
+//remove it in production
+app.use(morgan("dev"))
+
+app.use("api/v1", appRouter)
 
 export default app
